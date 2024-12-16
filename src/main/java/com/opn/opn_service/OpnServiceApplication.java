a670a23863ec289cb4cc0cda4ed63a4583960d19
Samiylo <skryshu@gmail.com>
2024-12-15 20:53:38 -0600

    Initialize Source files

---
+++ src/main/java/com/opn/opn_service/OpnServiceApplication.java
@@ -0,0 +1,13 @@
+package com.opn.opn_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpnServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpnServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opn/opn_service/OpnServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opn.opn_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpnServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
