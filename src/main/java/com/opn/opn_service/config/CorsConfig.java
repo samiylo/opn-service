334d32da719473033824f636a87bde15412d613b
Samiylo <skryshu@gmail.com>
2024-12-24 17:25:50 -0600

    WebConfig: Enable Cors

---
+++ src/main/java/com/opn/opn_service/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.opn.opn_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CorsConfig implements WebFluxConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://samiylo.github.io/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
